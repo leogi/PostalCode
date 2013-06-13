c6110a07f69eb69e197cab531293b2679c4aa0ac
levannghia <nghialv.bk@gmail.com>
2013-06-13 16:42:12 +0700

    Fix import data uncorrectly

--- lib/postal_code/import.rb
+++ lib/postal_code/import.rb
@@ -6,12 +6,12 @@ module PostalCode
     private
 
     def import_postal_code filepath = DEFAULT_POSTAL_CODE_CSV_FILE
-      csv = CSV.table(File.new(filepath), encoding: "SJIS")
+      csv = CSV.read(File.new(filepath), headers: false, encoding: "SJIS")
       postal_codes = []
       csv.each do |row|
         attributes = {}
         PostalCode::Postal::ATTRIBUTES.each_with_index{|name, index|
-          attributes.merge!(name => row[index])}
+          attributes.merge!(name => (index == 8 ? row[index].delete(' ') : row[index]))}
         postal_codes << PostalCode::Postal.new(attributes)
       end
       postal_codes
